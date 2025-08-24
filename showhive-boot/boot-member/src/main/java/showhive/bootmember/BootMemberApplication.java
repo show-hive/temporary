9c54baf3f03dc5adb79d5f6fda8f738547ca8047
Jiihyun <cwlgusc@gmail.com>
2025-08-24 23:32:49 +0900

    chore: 루트 모듈 내 lombok 의존성 추가

--- showhive-boot/boot-member/src/main/java/showhive/bootmember/BootMemberApplication.java
+++ showhive-boot/boot-member/src/main/java/showhive/bootmember/BootMemberApplication.java
@@ -2,28 +2,11 @@ package showhive.bootmember;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import showhive.datamember.domain.Member;
-import org.springframework.transaction.annotation.*;
-import showhive.infrapersistence.member.JpaMemberRepository;
 
-@SpringBootApplication(
-        // 컴포넌트 스캔: showhive 전체 하위 패키지 스캔 (Controller/Service 등)
-        scanBasePackages = "showhive"
-)
-@EnableTransactionManagement                   // @Transactional 활성화
-@EnableJpaAuditing                               // @CreatedDate, @LastModifiedDate 등 감사 필드 자동 설정
-@EntityScan(basePackageClasses = {               // 엔티티 스캔(문자열 대신 클래스 기준 권장)
-        Member.class
-})
-@EnableJpaRepositories(basePackageClasses = {    // 스프링데이터 JPA 리포 스캔
-        JpaMemberRepository.class
-})public class BootMemberApplication {
+@SpringBootApplication
+public class BootMemberApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootMemberApplication.class, args);
     }
-
 }
---
+++ showhive-infra/infra-persistence/src/main/java/showhive/infrapersistence/config/InfraPersistenceConfig.java
@@ -0,0 +1,14 @@
+//package showhive.infrapersistence.config;
+//
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//
+//@Configuration
+//@EntityScan(basePackages = "showhive.datamember.domain")
+//@ComponentScan(basePackages = "showhive.infrapersistence.member")
+//@EnableJpaRepositories(basePackages = "showhive.infrapersistence.member")
+//public class InfraPersistenceConfig {
+//}
+//
